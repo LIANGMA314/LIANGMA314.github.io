d9328a058c271f3ab1e8f0446983100d78e6b158
LIANGMA314 <ml763993478@gmail.com>
2020-05-15 21:45:30 +0800

    add new model of live 2d widget

--- gulpfile.js
+++ gulpfile.js
@@ -86,3 +86,19 @@ gulp.task('watch', function() {
 });
 
 gulp.task('default', ['js', 'sass', 'fonts', 'browser-sync', 'watch']);
+
+var pio = new Paul_Pio({
+    "mode": "fixed",
+    "hidden": true,
+    "content": {
+        "welcome": ["欢迎来到保罗的小宇宙！", "今天天气不错，一起来玩吧！", "博主每天都有些折腾记录，欢迎前往他的小窝阅读~"],
+        "custom": [
+            {"selector": ".comment-form", "text": "欢迎参与本文评论，别发小广告噢~"},
+            {"selector": ".home-social a:last-child", "text": "在这里可以了解博主的日常噢~"},
+            {"selector": ".post-item a", "type": "read"},
+            {"selector": ".post-content a, .page-content a", "type": "link"}
+        ]
+    },
+    "night": "single.night()",
+    "model": ["https://paugram.com/usr/plugins/Pio/models/yuan/model.json"]
+});
